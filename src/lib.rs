b125c3e9e1d96c819f4ff20aafa627bee3d07985
Jeremy Davis <jeremydavis519@gmail.com>
2022-10-25 23:36:13 -0500

    Rename and extend `emulate_macro_expansion`.
    It is now `emulate_functionlike_macro_expansion`, in preparation for supporting other types of macros. It also
    supports expanding multiple macros with the same function call by passing a slice of path/function tuples.

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,11 @@
-// Copyright (c) 2018 Jeremy Davis (jeremydavis519@gmail.com)
-// 
+// Copyright (c) 2018-2022 Jeremy Davis (jeremydavis519@gmail.com)
+//
 // Licensed under the Apache License, Version 2.0 (located at /LICENSE-APACHE
 // or http://www.apache.org/licenses/LICENSE-2.0), or the MIT license
 // (located at /LICENSE-MIT or http://opensource.org/licenses/MIT), at your
 // option. The file may not be copied, modified, or distributed except
 // according to those terms.
-// 
+//
 // Unless required by applicable law or agreed to in writing, this software
 // is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
 // ANY KIND, either express or implied. See the applicable license for the
@@ -14,43 +14,51 @@
 //! This crate offers a way to emulate the process of procedural macro expansion at run time.
 //! It is intended for use with code coverage tools like [`tarpaulin`], which can't measure
 //! the code coverage of anything that happens at compile time.
-//! 
+//!
 //! Currently, `runtime-macros` only works with `functionlike!` procedural macros. Custom
 //! derive may be supported in the future if there's demand.
-//! 
+//!
 //! [`tarpaulin`]: https://crates.io/crates/cargo-tarpaulin
-//! 
+//!
 //! To use it, add a test case to your procedural macro crate that calls `emulate_macro_expansion`
 //! on a `.rs` file that calls the macro. Most likely, all the files you'll want to use it on will
 //! be in your `/tests` directory. Once you've completed this step, any code coverage tool that
 //! works with your crate's test cases will be able to report on how thoroughly you've tested the
 //! macro.
-//! 
+//!
 //! See the `/examples` directory in the [repository] for working examples.
-//! 
+//!
 //! [repository]: https://github.com/jeremydavis519/runtime-macros
 
-extern crate proc_macro2;
+extern crate proc_macro;
+extern crate quote;
 extern crate syn;
 
-use std::fs;
-use std::io::Read;
-use std::panic::{self, AssertUnwindSafe};
+use {
+    std::{
+        fs,
+        io::Read,
+        panic::{self, AssertUnwindSafe}
+    },
+    quote::ToTokens
+};
 
-/// Parses the given Rust source code file, searching for macro expansions that use `macro_path`.
-/// Each time it finds one, it calls `proc_macro_fn`, passing it the inner `TokenStream` just as
-/// if the macro were being expanded. The only effect is to verify that the macro doesn't panic,
-/// as the expansion is not actually applied to the AST or the source code.
-/// 
+/// Parses the given Rust source code file, searching for functionlike macro calls that use each
+/// of the given macro paths. Each time it finds one, it calls the corresponding procedural macro
+/// function, passing it the inner `TokenStream` just as if the macro were being expanded. The only
+/// effect is to verify that the macro doesn't panic, as the expansion is not actually applied to
+/// the AST or the source code.
+///
 /// Note that this parser only handles Rust's syntax, so it cannot resolve paths to see if they
 /// are equivalent to the given one. The paths used to reference the macro must be exactly equal
 /// to the one given in order to be expanded by this function. For example, if `macro_path` is
 /// `"foo"` and the file provided calls the macro using `bar::foo!`, this function will not know
 /// to expand it, and the macro's code coverage will be underestimated.
-/// 
-/// Also, this function uses `proc_macro2::TokenStream`, not the standard but partly unstable
-/// `proc_macro::TokenStream`. You can convert between them using their `into` methods, as shown
-/// below.
+///
+/// Also, this function uses `proc_macro2::TokenStream`, not the standard `proc_macro::TokenStream`.
+/// The Rust compiler disallows using the `proc_macro` API for anything except defining a procedural
+/// macro (i.e. we can't use it at runtime). You can convert between the two types using their
+/// `into` methods, as shown below.
 ///
 /// # Returns
 ///
@@ -58,67 +66,74 @@ use std::panic::{self, AssertUnwindSafe};
 /// read or parse the file.
 ///
 /// [`Error`]: enum.Error.html
-/// 
+///
 /// # Example
-/// 
-/// ```ignore
-/// # // This example doesn't compile because procedural macros can only be made in crates with
-/// # // type "proc-macro".
-/// # #![cfg(feature = "proc-macro")]
-/// # extern crate proc_macro;
-/// # extern crate proc_macro2;
+///
+/// ```
+/// # /*
 /// #[proc_macro]
-/// fn remove(_: proc_macro::TokenStream) -> proc_macro::TokenStream {
-///     // This macro just eats its input and replaces it with nothing.
-///     proc_macro::TokenStream::empty()
+/// fn remove(ts: proc_macro::TokenStream) -> proc_macro::TokenStream {
+///     // This stub just allows us to use `proc_macro2` instead of `proc_macro`.
+///     remove_internal(ts.into()).into()
 /// }
-/// 
+/// # */
+///
+/// fn remove_internal(_: proc_macro2::TokenStream) -> proc_macro2::TokenStream {
+///     // This macro just eats its input and replaces it with nothing.
+///     proc_macro2::TokenStream::empty()
+/// }
+///
 /// extern crate syn;
-/// 
+///
 /// #[test]
 /// fn macro_code_coverage() {
 ///     let file = std::fs::File::open("tests/tests.rs");
-///     emulate_macro_expansion(file, "remove", |ts| remove(ts.into()).into());
+///     emulate_functionlike_macro_expansion(file, &[("remove", remove_internal)]);
 /// }
 /// ```
-pub fn emulate_macro_expansion<F>(mut file: fs::File, macro_path: &str, proc_macro_fn: F)
-        -> Result<(), Error>
+pub fn emulate_functionlike_macro_expansion<'a, F>(
+        mut file: fs::File,
+        macro_paths_and_proc_macro_fns: &[(&'a str, F)]
+) -> Result<(), Error>
         where F: Fn(proc_macro2::TokenStream) -> proc_macro2::TokenStream {
-    struct MacroVisitor<F: Fn(proc_macro2::TokenStream) -> proc_macro2::TokenStream> {
-        macro_path: syn::Path,
-        proc_macro_fn: AssertUnwindSafe<F>
+    struct MacroVisitor<'a, F: Fn(proc_macro2::TokenStream) -> proc_macro2::TokenStream> {
+        macro_paths_and_proc_macro_fns: AssertUnwindSafe<Vec<(syn::Path, &'a F)>>
     }
-    impl<'ast, F> syn::visit::Visit<'ast> for MacroVisitor<F>
+    impl<'a, 'ast, F> syn::visit::Visit<'ast> for MacroVisitor<'a, F>
             where F: Fn(proc_macro2::TokenStream) -> proc_macro2::TokenStream {
         fn visit_macro(&mut self, macro_item: &'ast syn::Macro) {
-            if macro_item.path == self.macro_path {
-                (*self.proc_macro_fn)(macro_item.tts.clone());
+            for (path, proc_macro_fn) in self.macro_paths_and_proc_macro_fns.iter() {
+                if macro_item.path == *path {
+                    proc_macro_fn(macro_item.tokens.clone().into());
+                }
             }
         }
     }
-    
-    let proc_macro_fn = AssertUnwindSafe(proc_macro_fn);
 
     let mut content = String::new();
     file.read_to_string(&mut content).map_err(|e| Error::IoError(e))?;
-    
+
     let ast = AssertUnwindSafe(syn::parse_file(content.as_str()).map_err(|e| Error::ParseError(e))?);
-    let macro_path: syn::Path = syn::parse_str(macro_path).map_err(|e| Error::ParseError(e))?;
+    let macro_paths_and_proc_macro_fns = AssertUnwindSafe(
+        macro_paths_and_proc_macro_fns.iter()
+            .map(|(s, f)| Ok((syn::parse_str(s)?, f)))
+            .collect::<Result<Vec<(syn::Path, &F)>, _>>()
+            .map_err(|e| Error::ParseError(e))?
+    );
 
     panic::catch_unwind(|| {
         syn::visit::visit_file(&mut MacroVisitor::<F> {
-            macro_path,
-            proc_macro_fn
+            macro_paths_and_proc_macro_fns
         }, &*ast);
-    }).map_err(|_| Error::ParseError(syn::parse::Error::new(proc_macro2::Span::call_site(), "macro expansion panicked")))?;
+    }).map_err(|_| Error::ParseError(syn::parse::Error::new(
+        proc_macro2::Span::call_site().into(), "macro expansion panicked"
+    )))?;
 
     Ok(())
 }
 
-/// The error type for [`emulate_macro_expansion`]. If anything goes wrong during the file loading
+/// The error type for `emulate_*_macro_expansion`. If anything goes wrong during the file loading
 /// or macro expansion, this type describes it.
-///
-/// [`emulate_macro_expansion`]: fn.emulate_macro_expansion.html
 #[derive(Debug)]
 pub enum Error {
     IoError(std::io::Error),
@@ -151,7 +166,7 @@ mod tests {
     use std::{env, time};
 
     #[test]
-    fn proc_macro_coverage() {
+    fn functionlike_proc_macro_coverage() {
         let mut config = Config::default();
         let test_dir = env::current_dir().unwrap().join("examples").join("custom_assert");
         config.manifest = test_dir.join("Cargo.toml");
